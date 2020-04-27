f6e889d729094d5731996447052875999c779dee
Kristi Gribble <kgribble4083@gmail.com>
2020-04-26 21:05:45 -0500

    added restricted code

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -5,4 +5,16 @@ const secret = require("../config/secret")
 module.exports = (req, res, next) => {
 
     const token = req.headers.authorization
+    if (token) {
+        jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: "No access granted", err })
+            } else {
+                res.decodedJwt = decodedToken
+                next()
+            }
+        })
+    } else {
+        res.status(500).json({ message: "Error - not authorized" })
+    }
 }
