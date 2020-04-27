b7f5a9b9319aa38c6f7cf76fa75d7d1c99c27933
Kristi Gribble <kgribble4083@gmail.com>
2020-04-26 20:58:08 -0500

    server code added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+
+const userRouter = require("./user/user-router.js")
+const authRouter = require("./auth/auth-router.js")
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.use("/users", restricted, userRouter)
+server.use("/auth", authRouter)
+
+server.get("/", (req, res) => {
+    res.json({ api: "IT'S ALIVE" })
+})
+
+module.exports = server;
